92fc9abc3b068b9a757713b5eae3c63c19fee919
uvatandost <uvatandost@my.harrisburgu.edu>
2021-03-29 16:17:58 -0400

    configurations

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, request, redirect
 
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['Alpha-Vantage-API-key']
+
+import requests
+import json 
+key = s3
+ticker = 'AAPL'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
+response = requests.get(url)
+print(response.json()) 
+
 app = Flask(__name__)
 
 @app.route('/')
